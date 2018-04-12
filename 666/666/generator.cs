f68c50a0f4ab494d3f7b747ad9499fa6e7a39cb8
Admin <Admin@DESKTOP-INJ064T>
2018-04-12 09:23:27 +0700

    sec

---
+++ 666/666/generator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _666
+{
+    class generator
+    {
+        public int[,] field;
+        int N;
+       
+
+        public void init(int n)
+        {
+            N = n;
+            field = new int[n, n];
+        }
+
+        public void plantMines(int k)
+        {
+            Random rand = new Random();
+      
+            for (int i = 0; i < k; i++)
+            {   
+                    field[rand.Next(0, field.GetLength(0)), rand.Next(0, field.GetLength(1))] = 1;
+            }
+
+        }
+
+        public void calculate()
+        {
+
+        }
+
+    }
+}
